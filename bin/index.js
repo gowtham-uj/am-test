2a1ab8d4ea471257c7f3424f27dbb2e8cc63c89f
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-03-13 14:31:05 +0530

    testing solutions

--- bin/index.js
+++ bin/index.js
@@ -18,7 +18,7 @@ let url = argv.url;
 let mocha = new Mocha();
 
 var bootstrapPath = path.dirname(require.resolve("am-test/package.json"));
-console.log(bootstrapPath);
+// console.log(bootstrapPath);
 
 mocha.addFile(testPath);
 
