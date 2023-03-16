346629b192604f390a98f33f47c1a16055383274
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-03-16 15:39:37 +0530

    changing reporter

--- bin/index.js
+++ bin/index.js
@@ -5,35 +5,36 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 const Mocha = require("mocha");
 const path = require("path");
-const mochawesome = require("mochawesome");
 
-let mocha = new Mocha({
-  reporter: "mochawesome",
-  reporterOptions: {
-    // reportFilename: "customReportFilename",
-    reportFilename: "[status]_[datetime]-[test-xyz]-report",
-    quiet: true,
-    html: true,
-    json: true,
-  },
-});
-
-// path.join(path.dirname(require.resolve("am-test/package.json")), "test.js");
-const testFilePath = path.join(
-  path.dirname(require.resolve("am-test/package.json")),
-  "test.js"
-);
 // console.log();
 
 if (require.main === module) {
-  // console.log("called directly");
-
+  let doesStoreOut = null;
   if (!argv.url) {
     console.log(`please provide the url of the live app to test the app.
   ex : am-test --url="somexample.com" with no last / of that url`);
     process.exitCode = 1;
     process.exit();
   }
+  if (!argv.outputDest) {
+    doesStoreOut = false;
+  } else if (argv.outputDest) {
+    doesStoreOut = true;
+  }
+  // console.log("called directly");
+
+  let mocha = new Mocha({
+    reporter: "mocha-simple-html-reporter",
+    reporterOptions: {
+      output: doesStoreOut ? argv.outputDest : null,
+    },
+  });
+
+  // path.join(path.dirname(require.resolve("am-test/package.json")), "test.js");
+  const testFilePath = path.join(
+    path.dirname(require.resolve("am-test/package.json")),
+    "test.js"
+  );
 
   let url = argv.url;
 
