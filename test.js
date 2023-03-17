35d54b5956f16adcbe624cbd4642cb4cd15b58aa
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-03-17 10:08:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -265,6 +265,7 @@ describe("starting to execute the tests", async function () {
   this.timeout(100000);
   this.beforeAll(async () => {
     // let test = new BBTest("https://dummy-assign-mentor.onrender.com");
+    console.log(argv.url);
     let test = new BBTest(argv.url);
     let res = await test.AMTaskTests();
   });
