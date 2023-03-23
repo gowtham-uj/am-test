80fe4768dff5767a4dc33271effe0dab41b9fe68
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-03-23 12:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ if (require.main === module) {
   mocha.addFile(testFilePath);
 
   mocha.run(async function (failures) {
-    this.timeout(30000);
+    this.timeout(50000);
     // process.exitCode = failures ? 1 : 0; // exit with non-zero status if there were failures
 
     // take the markdown file and create the pdf with it
