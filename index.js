e9db0e997fee712b6ddf4ab7e56ab6350c1a5d1d
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-03-31 23:14:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ if (require.main === module) {
   } else if (argv.saveOutput == "false") {
     doesStoreOut = false;
   }
-  console.log(argv);
+  console.log(argv.outputDest.replace(".pdf", ".json"));
   let mocha;
   if (argv.saveOutput == "true") {
     mocha = new Mocha({
