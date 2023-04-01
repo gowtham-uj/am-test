c09c36fbb4e485a7d1f958e8351de7730a50b392
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-04-01 16:30:20 +0530

    trail and error

--- index.js
+++ index.js
@@ -166,7 +166,26 @@ if (require.main === module) {
 
     mocha.addFile(testFilePath);
 
-    await mocha.run();
+    await mocha
+      .run()
+      .on("test", function (test) {
+        console.log("Test started: " + test.title);
+      })
+      .on("test end", function (test) {
+        console.log("Test done: " + test.title);
+      })
+      .on("pass", function (test) {
+        console.log("Test passed");
+        console.log(test);
+      })
+      .on("fail", function (test, err) {
+        console.log("Test fail");
+        console.log(test);
+        console.log(err);
+      })
+      .on("end", function () {
+        console.log("All done");
+      });
   }
   module.exports = { runTests };
 }
