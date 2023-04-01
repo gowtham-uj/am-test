8231de01a7ed0d35d84132c85febc8d1b2f1f44f
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-04-01 12:36:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ if (require.main === module) {
   let mocha = new Mocha({
     reporter: "mochawesome",
     reporterOptions: {
-      reportDir: path.resolve("~/dev/test/"),
+      reportDir: path.resolve("/home/sword/dev/test/"),
       reportFilename: "test",
       quiet: true,
       json: true,
